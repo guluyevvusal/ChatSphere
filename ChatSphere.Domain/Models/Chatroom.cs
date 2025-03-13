bb7b5fbaea223a5eeb344ef5be44a684625b557e
vüsal <quluyevvusal208@gmail.com>
2025-03-13 23:38:23 +0400

    Add project files.

---
+++ ChatSphere.Domain/Models/Chatroom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSphere.Domain.Models
+{
+    public class Chatroom
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public virtual ICollection< Message> Messages { get; set; }
+    }
+}
---
+++ ChatSphere.SiqnalR/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSphere.SiqnalR
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message, string chatRoom)
+        {
+            await Clients.Group(chatRoom).SendAsync("Receive Message", user, message);
+        }
+
+        public async Task JoinRoom(string chatRoom)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatRoom);
+            await Clients.Group(chatRoom).SendAsync("UserJoined", Context.ConnectionId);
+
+        }
+    }
+}
